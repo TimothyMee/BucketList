7e33c4501b1b4baaeabd3f2e2e292be3041f4103
Timothy_Mee <timothy33.tf@gmail.com>
2019-08-22 14:34:09 +0100

    connected mongodb

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+//database connection
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API Running");
