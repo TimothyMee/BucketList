4d7dccbf78b998cf3e79461bde2070b7ab4b1432
Timothy_Mee <timothy33.tf@gmail.com>
2019-08-22 18:50:21 +0100

    implemented - get all bucket list - get bucket list by id

--- controllers/BucketController.js
+++ controllers/BucketController.js
@@ -30,11 +30,37 @@ const insertBucketList = async (req, res) => {
 };
 
 const getAllBucketLists = async (req, res) => {
-  res.send("here");
+  try {
+    //verify user
+    const user = User.findById(req.user.id);
+    if (!user) return res.status(401).json({ msg: "unauthorized user token" });
+
+    const bucketlist = BucketList.find().sort({ date: -1 });
+    if (!bucketlist)
+      return res.status(400).json({ msg: "no bBucketlist found" });
+
+    res.status(200).json(bucketlist);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
 };
 
 const getBucketListById = async (req, res) => {
-  res.send("here");
+  try {
+    //verify user
+    const user = User.findById(req.user.id);
+    if (!user) return res.status(401).json({ msg: "unauthorized user token" });
+
+    const bucketList = BucketList.findById(req.param.id);
+    if (!bucketList)
+      return res.status(400).json({ msg: "No bucketlist found" });
+
+    res.status(200).json(bucketList);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send("Server Error");
+  }
 };
 
 const updateBucketListWithId = async (req, res) => {
