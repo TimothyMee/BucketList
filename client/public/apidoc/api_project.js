aa8ac39dea82721aefe9f5f8a024096f7834d3b2
Timothy_Mee <timothy33.tf@gmail.com>
2019-08-26 19:26:01 +0100

    finished api docs

---
+++ client/public/apidoc/api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "BucketList API documentation",
+  "version": "1.0.0",
+  "description": "API BucketList manager",
+  "template": {
+    "forceLanguage": "en"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-08-26T10:44:49.046Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
