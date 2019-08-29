b4a3ae21eb71d6b4766517faafcb0ee26d5e3693
Timothy_Mee <timothy33.tf@gmail.com>
2019-08-29 16:34:27 +0100

    git removed a file from old commits

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 //database connection
 connectDB();
 
 app.use(express.json({ extended: false }));
+const staticFiles = express.static(path.join(__dirname, "client/build"));
+app.use(staticFiles);
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 app.get("/", (req, res) => {
   res.send("API Running");
 });
